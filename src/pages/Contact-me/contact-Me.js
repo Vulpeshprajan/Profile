c019ac3adc9ebd6005f76930f921f2bd65c0efdf
Sagar Pyakurel <pyakurelsagar35@gmail.com>
2022-02-15 17:46:42 +1100

    profile complete

--- src/pages/Contact-me/contact-Me.js
+++ src/pages/Contact-me/contact-Me.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 import { Button, Col, Form, Row } from 'react-bootstrap'
 import DefaultLayout from '../layout/DefaultLayout'
+import social from "../../assets/social.jpg"
 import "./Contactme.style.css";
 
 
@@ -13,21 +14,7 @@ import "./Contactme.style.css";
   };
 
   export const Contact = ({ contactMe }) => {
-    const [frmData, setFrmData] = useState(initialForm);
-  
-    const handleOnChange = e => {
-      const { name, value } = e.target;
-      setFrmData({
-        ...frmData,
-        [name]: value,
-      });
-      console.log(name, value);
-    };
-  
-    const handleOnSubmit = e => {
-      // e.preventDefault();
-      contactMe(frmData);
-    };
+    
 
 
 
@@ -35,59 +22,65 @@ import "./Contactme.style.css";
         <DefaultLayout>
           
       
-            <Form onSubmit={handleOnSubmit} className="container " >
+          <Row>
+        <div className="container "  >
           <h1 ><strong> Lets talk  </strong></h1> <br />
-          <h5>Get in touch via the form below, or by emailing
+          <h5>Get in touch via the social media below
           
-          <a className="mail" href="mailto:pyakurelsagar35@gmail.com"  target="_blank" rel= "noreferrer"> pyakurelsagar35@gmail.com</a>
+          
           
           </h5> <hr />
-          <br/>
-  <div className="conatiner mb-3">
-    <Form.Group as={Col} controlId="formGridEmail">
-      <Form.Label> Name *</Form.Label>
-              <Form.Control
-                 name="name"
-                type="name"
-                value={frmData.name}
-                placeholder="Enter your name"
-                onChange={handleOnChange}
-                required />
-    </Form.Group> <br/>
-
-    <Form.Group as={Col} controlId="formGridPassword">
-      <Form.Label>Email * </Form.Label>
-              <Form.Control
-                name="email"
-                type="email"
-                value={frmData.email}
-                placeholder="Please enter your email "
-                onChange={handleOnChange}
-                required />
-            </Form.Group>
-            <br/>
-
-              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-    <Form.Label>Message</Form.Label>
-              <Form.Control
-                name="message"
-                type="message"
-                value={frmData.message}
-                as="textarea"
-                onChange={handleOnChange}
-                rows={5} />
-  </Form.Group>
-             <br/>
+            <br />
+            </div>
+            </Row>
+          
+        <Row>
+          <Col>
+          <img src={social} alt=" social media" width="80%" height="auto"/>
+          
+          </Col>
+          <Col >
+       
+             
+            <div className="profile" >    
+  
+        
             
-  
-  
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
- </div>
+                <div>
 
+         <a href="https://www.linkedin.com/in/sagar-pyakurel-617917174/" rel= "noreferrer" target="_blank"> <i className="fab fa-linkedin-in text-primary"></i> Sagar-pyakurel </a>
+                </div>
+                <br/>
+                
+                <div>         
+      <a href="https://github.com/Vulpeshprajan" target="_blank" rel="noreferrer"><i className="fab fa-github text-dark"></i> Sagar Pyakurel</a>
+              
+              </div>   
+              <br /> 
+
+                <div>
+                <a  href="https://www.facebook.com/prajan.ace" target="_blank" rel= "noreferrer"><i className="fab fa-facebook text-primary"></i> prajan.ace </a>
+
+                </div>
+                <br/>
+                
+
+                <div>
+                <a  href="mailto:pyakurelsagar35@gmail.com"  target="_blank" rel= "noreferrer"><i class="fas fa-envelope text-warning"></i> pyakurelsagar35@gmail.com </a>
+ 
+                </div>
+                <br/>
+                
+
+                <div>
+                <a href="https://twitter.com/prajanpyakurel"  target="_blank" rel= "noreferrer">  <i className="fab fa-twitter text-primary"></i> @prajanpyakurel </a>
+                </div>
+                </div>
+        
+          </Col>
+          </Row>   
   
-        </Form>
+        
         
         
    </DefaultLayout>
--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -3,6 +3,8 @@ import DefaultLayout from '../layout/DefaultLayout'
 import Cal from "../../assets/calculator.PNG";
 import NTD from "../../assets/ntd.PNG";
 import flex from "../../assets/flex.PNG";
+import "./projects.css";
+
 
 const Projects = () => {
     return (
@@ -22,15 +24,13 @@ const Projects = () => {
                 <img src={NTD} alt="need  " width="100%" height="auto"/>
                 </div>
                     <div className="col-md-7 order-md-1">
-                        <h2>To-DO Lists</h2>
+                        <h2>To-Do Lists</h2>
                         <div>
-                           <div className="fst-italic"><strong>Tech:</strong> HTML, CSS, JS
+                           <div><strong>Tech: Html, CSS, Js </strong> 
                         </div>
                         <p>
                             <strong>About: </strong>
-                            We use HTML, CSS and Javascript to develop this project which allow us to add things as to-do task. We have a button that triggers "onClick" even to shift to Not to-do Lists.
-                            We have functionality to add tasks and hours which are calculated in each section to-do list and
-                            Not to-do
+                                I use Html, CSS and Javascript to develop this project which allow us to add things as to-do task. A to-do list is a handy tool to help you stay more focused on the important things in your life and career.The basic features of this project is that we can add things that needs to do like a planner and can move to not to-do section if the task is already done. We have a button to move from to-do lists to not to-do lists and a button to delete tasks. We have functionality to add tasks and hours which are calculated in each section to-do list and not to-do
                             </p>
                         </div> 
                     </div>
@@ -46,14 +46,14 @@ const Projects = () => {
                     <div className="col-md-7 order-md-2">
                         <h2>Blog </h2>
                         <div>
-                            <div className="fst-italic">
-                                <strong>Tech: </strong> HTML, CSS
+                            <div >
+                                <strong>Tech:  Html, CSS </strong>
                                
                         </div>
                         <p>
                                 <strong>About: </strong>
                                 
-                                    For this webiste we use basic HTML and CSS. We design some nav bar, logo and list items in a blog.
+                                    For this webiste I use basic HTML and CSS. I design some nav bar, logo and list items in a blog. It's a simple website design made when I first started to code.
                                 
                         </p>
                         </div> 
@@ -70,13 +70,13 @@ const Projects = () => {
                     <div className="col-md-7 order-md-1">
                         <h2>Calculator</h2>
                         <div>
-                           <div className="fst-italic"><strong>Tech:</strong> HTML, CSS, JS
+                           <div ><strong>Tech: Html, CSS, Js </strong>
                         </div>
                         <p>
                             <strong>About: </strong>
-                                For this project I use basic component HTML, CSS & JavaScript.
-                                We have the same functionality of a normal calulator as we can do all the operation of basic calculator.
-                                The only unique functionality is that the calculator generate random value with the help of JavaScript.
+                                For this project I use basic component Html, CSS & JavaScript.
+                                The project have the same functionality of a normal calulator where user can do all the operation of basic calculator.
+                                The only unique functionality is that the calculator is made for fun as a prank calulator which generate random value with the help of JavaScript.
                             </p>
                         </div> 
                     </div>
